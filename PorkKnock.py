9464d3d24cd0f1b720c9e185703fb5ce5030273c
Daniel-Walpole <57097166+Daniel-Walpole@users.noreply.github.com>
2020-02-25 11:55:11 -0500

    Update PorkKnock.py

--- PorkKnock.py
+++ PorkKnock.py
@@ -7,19 +7,17 @@ ports = [5260,12345,36031]
 
 def main():
     print ("[+] Sequence is %s" % ports)
-    # Lets knock all of the possible combinations of the ports list
     for port in ports:
         print ("[+] Knocking on port %s:%s" % (destination,port))
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
+        sock.settimeout(5)
         sock.connect_ex((destination, port))
         sock.close()
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connection.connect_ex((destination,42020))
-    connection.settimeout(100)
+    while(True):
+        connection.connect_ex((destination,42020))
+        connection.settimeout(100)
     connection.close()
 
 if __name__ == '__main__':
-    print ("[+] Knock knock opener")
     main()
-    print ("[+] Done")
