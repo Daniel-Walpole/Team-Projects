66c65f992807453f6b98b17eac00f72627b649ec
Daniel-Walpole <57097166+Daniel-Walpole@users.noreply.github.com>
2020-02-27 09:23:41 -0500

    Update PorkKnock.py

--- PorkKnock.py
+++ PorkKnock.py
@@ -2,22 +2,22 @@ import socket
 import itertools
 import sys
 
-destination = "192.168.56.203"
-ports = [5260,12345,36031]
+destination = "10.16.189.81"
+ports = [5260,12345,36031,42020]
 
 def main():
     print ("[+] Sequence is %s" % ports)
     for port in ports:
         print ("[+] Knocking on port %s:%s" % (destination,port))
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(5)
-        sock.connect_ex((destination, port))
-        sock.close()
-    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    while(True):
-        connection.connect_ex((destination,42020))
-        connection.settimeout(100)
-    connection.close()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((destination, port))
+            s.sendall(b'Y')
+            s.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as f:
+        print ("[+] Knocking on port %s:%s" % (destination,42069))
+        f.connect_ex((destination, 42069))
+        f.sendall(b'12345678901234')
+        f.close()
 
 if __name__ == '__main__':
     main()
